402dd479e8c94483312ee2f489feec62cd312964
Marin Vasilev <mvasilev@axway.com>
2016-12-03 10:12:19 +0200

    Updating test cases

--- tests/authPlugin.js
+++ tests/authPlugin.js
@@ -8,18 +8,18 @@ var base = require('./_init'),
     github = require('../'),
     passport = new Object(); //will become a passport instance
 
-describe('Github Authentication Plugin', function () {
+describe('Github Authentication Plugin', function() {
     var auth;
 
-    before(function (next) {
+    before(function(next) {
         auth = new github(server);
         passport = new auth.getPassport();
         next();
     });
 
 
-    it('should throw error on invalid configuration', function (next) {
-        (function () {
+    it('should throw error on invalid configuration', function(next) {
+        (function() {
             var testServer = server;
             testServer.config = {};
             return new github(testServer);
@@ -27,12 +27,12 @@ describe('Github Authentication Plugin', function () {
         next();
     });
 
-    it('should passport be an object', function (next) {
+    it('should passport be an object', function(next) {
         should(passport).be.type('object');
         next();
     });
 
-    it('should attach passport to server', function (next) {
+    it('should attach passport to server', function(next) {
         var passportKeys = ['_key',
             '_strategies',
             '_serializers',
@@ -49,7 +49,7 @@ describe('Github Authentication Plugin', function () {
         next();
     });
 
-    it('should have valid API configutation', function (next) {
+    it('should have valid API configutation', function(next) {
         var apiConfigurationKeys = ['clientID',
             'clientSecret',
             'callbackURL',
@@ -59,22 +59,22 @@ describe('Github Authentication Plugin', function () {
         next();
     });
 
-    it('should match all urls', function (next) {
+    it('should match all urls', function(next) {
         should(auth.matchURL({})).be.true;
         next();
     });
 
     //Check the validateRequest funciton directly
-    it('should validate paths', function (next) {
+    it('should validate paths', function(next) {
         var req = {};
         var res = {};
         var authentication = true;
         req.url = '/wrongUri';
-        req.isAuthenticated = function () { return false; };
+        req.isAuthenticated = function() { return false; };
         auth.config.loginUrl = undefined;
         auth.config.allowedPaths = undefined;
         //check if error is thrown
-        (function () {
+        (function() {
             try {
                 authentication = auth.validateRequest(req, res);
             } catch (E) {
@@ -92,37 +92,45 @@ describe('Github Authentication Plugin', function () {
     it('should validate Requests', function validatesRequests(next) {
         //Url to the foo Route registered in app.js
         var url = 'http://localhost:' + server.port + '/fooMe';
+        //First off, lets check if the correct auth type is set
+        should(auth.config.APIKeyAuthType).be.eql('plugin');
+        should(auth.config.APIKeyAuthPlugin).be.eql('appc.githubauth'); // yes, that's a hack :)
         //And now, let's make an unauthenticated request
         request({
             method: 'GET',
             url: url,
             json: true
-        }, function (err, response, body) {
+        }, function(err, response, body) {
             //Request should not be authenticated
-            console.log(body);
             should(body.success).be.false;
             should(body.message).containEql('Unauthorized');
             next();
         });
     });
 
-    // it('should allow access to pre-configured paths', function prePaths(next) {
-    //     //Url to the foo Route registered in app.js
-    //     var url = 'http://localhost:' + server.port + '/youFoo';
-    //     //First off, lets check if the correct auth type is set
-    //     should(server.config.APIKeyAuthType).be.eql('plugin');
-    //     should(server.config.APIKeyAuthPlugin).be.eql('index.js'); // yes, that's a hack :)
-    //     //And now, let's make an unauthenticated request, to an allowed (in config) route
-    //     request({
-    //         method: 'GET',
-    //         url: url,
-    //         json: true
-    //     }, function (err, response, body) {
-    //         //Request should not be authenticated
-    //         should(body.success).be.true;
-    //         should(body).containEql('<p>You are now authenticated!</p>');
-    //         next();
-    //     });
-    // });
+    it('should allow access to pre-configured paths', function prePaths(next) {
+        //Url to the foo Route registered in app.js
+        var url = 'http://localhost:' + server.port + '/youFoo';
+        //First off, lets check if the correct auth type is set
+        should(auth.config.APIKeyAuthType).be.eql('plugin');
+        should(auth.config.APIKeyAuthPlugin).be.eql('appc.githubauth'); // yes, that's a hack :)
+        //And now, let's make an unauthenticated request, to an allowed (in config) route
+        request({
+            method: 'GET',
+            url: url,
+            json: true
+        }, function(err, response, body) {
+            //Request should not be authenticated
+            should(body.success).be.true;
+            should(body).containEql('<p>You are now authenticated!</p>');
+            next();
+        });
+    });
+
+    it('should return the correct instance', function(next){
+        var _passport = auth.getPassport();
+        console.log( (_passport instanceof Plugin));
+        next();
+    })
 
 });
